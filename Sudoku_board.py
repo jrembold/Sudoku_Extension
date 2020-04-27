d35a31f1cdd9a05e7849fad3bef6df66f98cc9d8
tthickman <60200700+tthickman@users.noreply.github.com>
2020-04-27 15:13:00 -0700

    Add files via upload

--- Sudoku_board.py
+++ Sudoku_board.py
@@ -41,30 +41,30 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
 
         self.shape_list = None
-        self.num_key = arcade.Sprite()
+        self.num_key = 0
 
-        self.num_list = arcade.SpriteList()
-        self.one = arcade.Sprite('Numbers/1.png', image_height=HEIGHT, image_width=WIDTH)
-        #center_x = SCREEN_WIDTH/2, center_y= SCREEN_HEIGHT/2,
-        self.two = arcade.Sprite('Numbers/2.png', image_height=HEIGHT, image_width=WIDTH)
-        self.three = arcade.Sprite('Numbers/3.png', image_height=HEIGHT, image_width=WIDTH)
-        self.four = arcade.Sprite('Numbers/4.png', image_height=HEIGHT, image_width=WIDTH)
-        self.five = arcade.Sprite('Numbers/5.png', image_height=HEIGHT, image_width=WIDTH)
-        self.six = arcade.Sprite('Numbers/6.png', image_height=HEIGHT, image_width=WIDTH)
-        self.seven = arcade.Sprite('Numbers/7.png', image_height=HEIGHT, image_width=WIDTH)
-        self.eight = arcade.Sprite('Numbers/8.png', image_height=HEIGHT, image_width=WIDTH)
-        self.nine = arcade.Sprite('Numbers/9.png', image_height=HEIGHT, image_width=WIDTH)
+        self.num_list = []
+        self.zero = arcade.Sprite('Numbers/0.png',  scale= 0.2)
+        self.one = arcade.Sprite('Numbers/1.png', scale= 0.2)
+        self.two = arcade.Sprite('Numbers/2.png', scale= 0.2)
+        self.three = arcade.Sprite('Numbers/3.png',scale= 0.2)
+        self.four = arcade.Sprite('Numbers/4.png',  scale= 0.2)
+        self.five = arcade.Sprite('Numbers/5.png',  scale= 0.2)
+        self.six = arcade.Sprite('Numbers/6.png',  scale= 0.2)
+        #self.seven = arcade.Sprite('Numbers/7.png', scale= 0.2)
+        #self.eight = arcade.Sprite('Numbers/8.png', scale= 0.2)
+        #self.nine = arcade.Sprite('Numbers/9.png',  scale= 0.2)
+        self.num_list.append(self.zero)
         self.num_list.append(self.one)
         self.num_list.append(self.two)
         self.num_list.append(self.three)
         self.num_list.append(self.four)
         self.num_list.append(self.five)
         self.num_list.append(self.six)
-        self.num_list.append(self.seven)
-        self.num_list.append(self.eight)
-        self.num_list.append(self.nine)
-        
-
+        #self.num_list.append(self.seven)
+        #self.num_list.append(self.eight)
+        #self.num_list.append(self.nine)
+        self.print_numlist = arcade.SpriteList()
 
 
         # Create a 2 dimensional array. A two dimensional
@@ -82,70 +82,71 @@ class MyGame(arcade.Window):
 
     #Changing the color of the squares and placing them
     def recreate_grid(self):
-        self.shape_list = arcade.ShapeElementList()
+        #self.shape_list = arcade.ShapeElementList()
+        print(self.num_key)
+        print(self.grid)
+        print(len(self.print_numlist))
+        self.print_numlist = arcade.SpriteList()
         for row in range(ROW_COUNT):
             for column in range(COLUMN_COUNT):
-                if self.grid[row][column] == 0:
-                    color = arcade.color.WHITE
-                else:
-                    color = arcade.color.GREEN
-
+                sprite = arcade.Sprite(f'Numbers/{self.grid[row][column]}.png', scale = 0.2)
+                
                 x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
                 y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
 
-                current_rect = arcade.create_rectangle_filled(x, y, WIDTH, HEIGHT, color)
-                self.shape_list.append(current_rect)
+                sprite.center_x = x
+                sprite.center_y = y
+                self.print_numlist.append(sprite)
 
     def on_draw(self):
         """
         Render the screen.
         """
-
         # This command has to happen before we start drawing
         arcade.start_render()
 
-        self.shape_list.draw()
-        self.num_list.draw()
+        #self.shape_list.draw()
+        self.print_numlist.draw()
 
     def on_key_press(self, key, mod):
         if key == arcade.key.ESCAPE:
             arcade.close_window()
         
-        elif key == arcade.key.NUM_1:
-            self.num_key = self.one
+        elif key == arcade.key.KEY_1:
+            self.num_key = 1
             return self.num_key
 
-        elif key == arcade.key.NUM_2:
-            self.num_key = self.two
+        elif key == arcade.key.KEY_2:
+            self.num_key = 2
             return self.num_key
 
-        elif key == arcade.key.NUM_3:
-            self.num_key = self.three
+        elif key == arcade.key.KEY_3:
+            self.num_key = 3
             return self.num_key
 
-        elif key == arcade.key.NUM_4:
-            self.num_key = self.four
+        elif key == arcade.key.KEY_4:
+            self.num_key = 4
             return self.num_key
 
-        elif key == arcade.key.NUM_5:
-            self.num_key = self.five
+        elif key == arcade.key.KEY_5:
+            self.num_key = 5
             return self.num_key
 
-        elif key == arcade.key.NUM_6:
-            self.num_key = self.six
+        elif key == arcade.key.KEY_6:
+            self.num_key = 6
             return self.num_key
 
-        elif key == arcade.key.NUM_7:
-            self.num_key = self.seven
-            return self.num_key
-
-        elif key == arcade.key.NUM_8:
-            self.num_key = self.eight
-            return self.num_key
-        
-        elif key == arcade.key.NUM_9:
-            self.num_key = self.nine
-            return self.num_key
+        #elif key == arcade.key.KEY_7:
+        #    self.num_key = 7
+        #    return self.num_key
+#
+        #elif key == arcade.key.KEY_8:
+        #    self.num_key = 8
+        #    return self.num_key
+        #
+        #elif key == arcade.key.KEY_9:
+        #    self.num_key = 9
+        #    return self.num_key
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
@@ -162,20 +163,17 @@ class MyGame(arcade.Window):
         # corner in the margin and go to a grid location that doesn't exist
         # AKA: make sure you are clicking w/in the grid - TH
         if row < ROW_COUNT and column < COLUMN_COUNT:
-            print(f'num_key is', int(self.num_key.center_x))
-            self.num_key.center_x = x
-            self.num_key.center_y = y
-
+            #print(f'Center-x of num_key', self.num_key.center_x, '. Center-y of num_key', self.num_key.center_y)
+            #print(f'Name of pic', str(self.num_key))
             # Flip the location between 1 and 0.
             # this will reset value for the recreate grid
             # and change the color  - TH
-            #if self.grid[row][column] == 0:
-            #    self.grid[row][column] = 1
-            #else:
-            #    self.grid[row][column] = 0
+            if self.grid[row][column] == 0:
+                self.grid[row][column] = self.num_key
+            else:
+                self.grid[row][column] = 0
 
-        self.recreate_grid() #creates the green grid
-        self.num_list.draw()
+        self.recreate_grid() 
 
 
 
