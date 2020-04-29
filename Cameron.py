486a8fc200b9ac2cb00e86356c42798039b59aaa
ctaggesell123 <60200686+ctaggesell123@users.noreply.github.com>
2020-04-29 13:22:35 -0700

    Update Cameron.py

--- Cameron.py
+++ Cameron.py
@@ -1,12 +1,12 @@
 abc = {'zero':0,'a': 1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6}
 
 # set deafult rows to 6 items
-r1 = [abc[4], abc[5], abc[6], abc[2], abc[1], abc[3]]
-r2 = [abc[3], abc[1], abc[2], abc[5], abc[4], abc[6]]
-r3 = [abc[5], abc[4], abc[3], abc[1], abc[6], abc[2]]
-r4 = [abc[6], abc[2], abc[1], abc[3], abc[5], abc[4]]
-r5 = [abc[2], abc[6], abc[5], abc[4], abc[3], abc[1]]
-r6 = [abc[1], abc[3], abc[4], abc[5], abc[2], abc[5]]
+r1 = [abc['d'], abc['e'], abc['f'], abc['b'], abc['a'], abc['c']]
+r2 = [abc['c'], abc['a'], abc['b'], abc['e'], abc['d'], abc['f']]
+r3 = [abc['e'], abc['d'], abc['c'], abc['a'], abc['f'], abc['b']]
+r4 = [abc['f'], abc['b'], abc['a'], abc['c'], abc['d'], abc['d']]
+r5 = [abc['b'], abc['f'], abc['e'], abc['d'], abc['c'], abc['a']]
+r6 = [abc['a'], abc['c'], abc['d'], abc['f'], abc['b'], abc['e']]
 
 # tuple of rows
 rows = (r1, r2, r3, r4, r5, r6)
