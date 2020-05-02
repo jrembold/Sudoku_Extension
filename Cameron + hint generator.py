00886add50a3f2af9467b653cd4de62755ce1134
cjhowland <60200853+cjhowland@users.noreply.github.com>
2020-05-02 02:42:47 -0700

    Add files via upload

---
+++ Cameron + hint generator.py
@@ -0,0 +1,175 @@
+### Generating the solution board ###
+
+import random
+
+# set numbers to letter in a dictionary so that we can shift the key 
+abc = {'a': 1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6}
+
+
+def Shift_dictionary( ):
+    '''
+    This function shifts the abc dictionary by a random number.
+
+    input:
+        none
+    output:
+        abc (dict): shifted num dictionary
+    '''
+    shift = random.randint(1,6)
+    for key in abc:
+        abc[key] += shift
+        if abc[key] > 6:
+            abc[key] -= 6
+    return abc
+
+
+def Apply_shift(abcnew):
+    '''
+    Applies shift to the known sudoku solution.
+
+    input:
+        abcnew (dict): shifted dictionary
+    output:
+        rows (tuple): tuple of lists with shifted row elements
+    '''
+    r1 = [abcnew['d'], abcnew['e'], abcnew['f'], abcnew['b'], abcnew['a'], abcnew['c']]
+    r2 = [abcnew['c'], abcnew['a'], abcnew['b'], abcnew['e'], abcnew['d'], abcnew['f']]
+    r3 = [abcnew['e'], abcnew['d'], abcnew['c'], abcnew['a'], abcnew['f'], abcnew['b']]
+    r4 = [abcnew['f'], abcnew['b'], abcnew['a'], abcnew['c'], abcnew['e'], abcnew['d']]
+    r5 = [abcnew['b'], abcnew['f'], abcnew['e'], abcnew['d'], abcnew['c'], abcnew['a']]
+    r6 = [abcnew['a'], abcnew['c'], abcnew['d'], abcnew['f'], abcnew['b'], abcnew['e']]
+    rows = [r1, r2, r3, r4, r5, r6]
+    return rows
+
+
+def Mix_rows(rowsnew):
+#     '''
+#     Mixes the rows. 
+
+#     input:
+#         rowsnew (tuple): tuple of rows with shifted values
+#     output:
+#         rowsmixed (tuple): the same rows but in a different order
+
+#     '''
+#     rowsmixed = ( )
+#     mix = random.randint(0,6)
+#     for i in range(0,6):
+#         row = i + 1
+#         if (mix + row) >= 6:
+#             newmix = mix - 6
+#             rowsmixed += (rowsnew[i+ newmix], )
+#         else:
+#             rowsmixed += (rowsnew[i+ mix], )
+
+#     return rowsmixed
+    return rowsnew
+
+
+def Generate_unique_board( ):
+    ''' 
+    runs the above functions
+
+    input:
+        none
+    output:
+        UniqueBoard (tuple): tuple of rows
+    '''
+    newdict = Shift_dictionary( )
+    shiftedabc = Apply_shift(newdict)
+    UniqueBoard = Mix_rows(shiftedabc)
+    f = open('answertext2.txt', 'w')
+    f.write(str(UniqueBoard))
+    f.close()
+    return UniqueBoard
+
+def read_text(textfile):
+    o = open(textfile, 'r')
+    for line in o:
+        fixed_answer = line
+
+    s = fixed_answer.split(',')
+    l1 = []
+    for i in s:
+        l1.append(i.strip(']'))
+    l2 = []
+    for q in l1:
+        l2.append(q.strip(' ['))
+    nums = []
+    for l in l2:
+        nums.append(int(l))
+
+    r1 = nums[:6]
+    r2 = nums[6:12]
+    r3 = nums[12:18]
+    r4 = nums[18:24]
+    r5 = nums[24:30]
+    r6 = nums[30:36]
+    all = [r1, r2, r3, r4, r5, r6]
+    return all
+
+
+
+
+###  Remove random numbers from solution to show the board  ###
+#sol = Generate_unique_board()
+def Partial_solution(solution, difficulty):
+    '''
+    Revomves 
+
+    '''
+    #difficulty = input("Input easy, medium or hard: ")
+    if difficulty == "easy":
+        amount_to_be_removed = 36 - random.randint(17,20)
+    elif difficulty == 'medium':
+        amount_to_be_removed = 36 - random.randint(13,17)
+    elif difficulty == 'hard':
+        amount_to_be_removed = 36 - random.randint(10,13)
+    partial = solution.copy()
+    for i in range(0, amount_to_be_removed):
+        randomi = random.randint(0,5)
+        randomj = random.randint(0,5)
+        partial[randomi][randomj] = 0
+    return partial
+
+
+#Generate_unique_board()
+full = read_text('answertext2.txt')
+part = Partial_solution(read_text('answertext2.txt'),'hard')
+#print(part)
+
+def Hint_Generator(partial,full):
+    blanks = ()
+    for i in range(0,6):
+        for j in range(0,6):
+            if partial[i][j] == 0:
+                blanks += ((i,j),)
+    coords = random.choice(blanks)
+    r = coords[0]
+    c = coords[1]
+    val = full[r][c]
+
+    partial[r][c] = val
+    return partial
+
+def Check_for_Completion(partial):
+    complete = False
+    for i in range(0,6):
+        for j in range(0,6):
+            if partial[i][j] == 0:
+                complete = False
+                break
+            else:
+                complete = True
+    return complete
+
+
+print(Check_for_Completion(part))
+
+#print(full)
+#print(Hint_Generator(part, full))
+
+
+
+
+
---
+++ Sudoku_board + hint generator.py
@@ -0,0 +1,222 @@
+"""
+Sudoku Board
+"""
+import arcade
+import Cameron2
+
+
+
+# Set how many rows and columns we will have
+ROW_COUNT = 6
+COLUMN_COUNT = 6
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 100
+HEIGHT = 100
+
+# This sets the margin between each cell
+# and on the edges of the screen.
+MARGIN = 6
+
+# Do the math to figure out our screen dimensions
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
+SCREEN_TITLE = "Suduko Board 6x6"
+print('Welcome to Arcade Sudoku!')
+print('Instructions:')
+print('- Press the number on your keyboard that you would like to enter.')
+print('- Click the box in which you would like to enter the number.')
+print('--------------------------------------------')
+print("| To generate a hint | Press the 'h' key   |")
+print("| To close the game  | Press the 'Esc' key |")
+print('--------------------------------------------')
+difficulty = input("To select difficulty level, input easy, medium, or hard: ")
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Set up the application.
+        """
+        ## INIT FUNCTION ##
+        super().__init__(width, height, title)
+
+        ## APPENDING THE SPRTIES ##
+        self.shape_list = None
+        self.num_key = 0
+
+        self.num_list = []
+        self.zero = arcade.Sprite('Numbers/0.png')
+        self.one = arcade.Sprite('Numbers/1.png') 
+        self.two = arcade.Sprite('Numbers/2.png')
+        self.three = arcade.Sprite('Numbers/3.png')
+        self.four = arcade.Sprite('Numbers/4.png')
+        self.five = arcade.Sprite('Numbers/5.png')
+        self.six = arcade.Sprite('Numbers/6.png')
+        #self.seven = arcade.Sprite('Numbers/7.png')
+        #self.eight = arcade.Sprite('Numbers/8.png')
+        #self.nine = arcade.Sprite('Numbers/9.png')
+        self.num_list.append(self.zero)
+        self.num_list.append(self.one)
+        self.num_list.append(self.two)
+        self.num_list.append(self.three)
+        self.num_list.append(self.four)
+        self.num_list.append(self.five)
+        self.num_list.append(self.six)
+        #self.num_list.append(self.seven)
+        #self.num_list.append(self.eight)
+        #self.num_list.append(self.nine)
+        self.print_numlist = arcade.SpriteList()
+
+        ## ANSWER KEY ##
+        self.answer = Cameron2.Generate_unique_board()
+        self.grid = Cameron2.Partial_solution(self.answer, difficulty)
+        self.fixed_answer = Cameron2.read_text('answertext2.txt')
+#        a = open('answertext2.txt','r')
+#        for line in a:
+#            self.fixed_answer = line
+#        a.close()
+
+        ## GENERATES BACKGROUND ##
+        arcade.set_background_color(arcade.color.BLACK)
+        self.recreate_grid()
+    
+
+
+    #Changing the color of the squares and placing them
+    def recreate_grid(self):
+        '''
+        This takes a list of numbers for each row/column
+        and populates a SpriteList based on what is in each row/column
+
+        '''
+        #self.shape_list = arcade.ShapeElementList()
+    #    print("Key pressed: ",self.num_key) #key pressed
+    #    print("Current: ",self.grid) #answer grid
+    #    print("Answer: ",self.fixed_answer)
+
+
+
+        self.print_numlist = arcade.SpriteList()
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                sprite = arcade.Sprite(f'Numbers/{self.grid[row][column]}.png', scale = 0.2)
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+                sprite.center_x = x
+                sprite.center_y = y
+                self.print_numlist.append(sprite)
+     #   print(Cameron2.Check_for_Completion(self.grid))
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        # This command has to happen before we start drawing
+        arcade.start_render()
+        
+
+        self.print_numlist.draw()
+
+        ## LINES SEPARATING THE GRID ##
+        arcade.draw_line(0, (2/3)*SCREEN_HEIGHT, SCREEN_WIDTH,(2/3)*SCREEN_HEIGHT, arcade.color.SILVER, line_width=7)
+        arcade.draw_line(0, (1/3)*SCREEN_HEIGHT, SCREEN_WIDTH,(1/3)*SCREEN_HEIGHT, arcade.color.SILVER, line_width=7)
+        arcade.draw_line((1/2)*SCREEN_WIDTH, 0, (1/2)*SCREEN_WIDTH,SCREEN_HEIGHT, arcade.color.SILVER, line_width=7)
+
+    def on_key_press(self, key, mod):
+        '''
+        The user will press a number on their keyboard
+        that number will be used to generate a new sprite
+        '''
+        ## Hint Generator
+        if key == arcade.key.H:
+            self.grid = Cameron2.Hint_Generator(self.grid, self.fixed_answer)
+            self.recreate_grid()
+
+        if key == arcade.key.ESCAPE:
+            arcade.close_window()
+
+        
+        ## NUMBERS TO PRESS TO GENERATE BOARD ##
+        elif key == arcade.key.KEY_1:
+            self.num_key = 1
+            return self.num_key
+
+        elif key == arcade.key.KEY_2:
+            self.num_key = 2
+            return self.num_key
+
+        elif key == arcade.key.KEY_3:
+            self.num_key = 3
+            return self.num_key
+
+        elif key == arcade.key.KEY_4:
+            self.num_key = 4
+            return self.num_key
+
+        elif key == arcade.key.KEY_5:
+            self.num_key = 5
+            return self.num_key
+
+        elif key == arcade.key.KEY_6:
+            self.num_key = 6
+            return self.num_key
+        
+        
+
+        #elif key == arcade.key.KEY_7:
+        #    self.num_key = 7
+        #    return self.num_key
+#
+        #elif key == arcade.key.KEY_8:
+        #    self.num_key = 8
+        #    return self.num_key
+        #
+        #elif key == arcade.key.KEY_9:
+        #    self.num_key = 9
+        #    return self.num_key
+    
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        The user will press a number on their keyboard
+        then that number will be placed on the board. 
+        """
+
+        # Change the x/y screen coordinates to grid coordinates
+        column = int(x // (WIDTH + MARGIN))
+        row = int(y // (HEIGHT + MARGIN))
+
+        print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
+
+        # Make sure we are on-grid. It is possible to click in the upper right
+        # corner in the margin and go to a grid location that doesn't exist
+        # AKA: make sure you are clicking w/in the grid - TH
+        if row < ROW_COUNT and column < COLUMN_COUNT:
+            #print(f'Center-x of num_key', self.num_key.center_x, '. Center-y of num_key', self.num_key.center_y)
+            #print(f'Name of pic', str(self.num_key))
+            # Flip the location between 1 and 0.
+            # this will reset value for the recreate grid
+            # and change the color  - TH
+            if self.grid[row][column] == 0:
+                self.grid[row][column] = self.num_key
+            else:
+                self.grid[row][column] = 0
+
+        self.recreate_grid() 
+
+
+
+
+def main():
+    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
